f1d4a154ae98808b6e7c1e0720e84dd9bcf41453
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-18 14:36:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
-def oxford_comma(array)
-  return array.join
-end
 
 def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
     return array.join(" and ")
-end
-def oxford_comma(array)
+  else
   return [array[0..-2].join(", "), array.last].join(", and ")
+end
 end
